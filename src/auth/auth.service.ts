dbdffa5c4e0d75f8a4395037b305b64a9082fa19
lu-perez <luperez@teco.com.ar>
2023-02-14 17:37:07 -0300

    validations & scripts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto/auth.dto';
 import * as argon from 'argon2';
@@ -8,20 +8,38 @@ export class AuthService {
   constructor(private prisma: PrismaService) {}
 
   async signUp(dto: AuthDto) {
-    console.log({ dto });
-
-    const hash = await argon.hash(dto.password);
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
-    delete user.hash;
-    return user;
+    try {
+      const hash = await argon.hash(dto.password);
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new ForbiddenException('Credentials taken');
+      }
+      throw error;
+    }
   }
 
-  signIn() {
-    return { message: 'Sign In' };
+  async signIn(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (!user) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+    const passwordMatches = await argon.verify(user.hash, dto.password);
+    if (!passwordMatches) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+    delete user.hash;
+    return user;
   }
 }
