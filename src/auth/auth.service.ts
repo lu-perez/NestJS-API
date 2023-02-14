49cd4c8b99ae01478a74692705ee721f0576bac5
lu-perez <luperez@teco.com.ar>
2023-02-14 15:56:57 -0300

    validators & models updated

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto/auth.dto';
+import * as argon from 'argon2';
 
 @Injectable({})
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signUp() {
-    return { message: 'Sign Up' };
+  async signUp(dto: AuthDto) {
+    console.log({ dto });
+
+    const hash = await argon.hash(dto.password);
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+    delete user.hash;
+    return user;
   }
 
   signIn() {
